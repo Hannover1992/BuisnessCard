297e10becf350a01d53547c91fed6a7800b26b77
Patryk Krzyzanski <hannover1992@googlemail.com>
2024-08-15 11:23:35 +0200

    scale down, and small margin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LetsWorkTogetherComponent } from './lets-work-together/lets-work-together.component';
+import { BuisnessCardBackComponent } from './buisness-card-back/buisness-card-back.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LetsWorkTogetherComponent],
+  imports: [
+    RouterOutlet,
+    LetsWorkTogetherComponent,
+    BuisnessCardBackComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
