c0f4fc8019b19cebc3580e9ae58d1b41a9f3564d
Patryk Krzyzanski <hannover1992@googlemail.com>
2024-08-15 09:58:13 +0200

    Visit Card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LetsWorkTogetherComponent } from './lets-work-together/lets-work-together.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LetsWorkTogetherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/lets-work-together/lets-work-together.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatDividerModule} from '@angular/material/divider';
+
+@Component({
+  selector: 'app-lets-work-together',
+  standalone: true,
+  imports: [MatDividerModule],
+  templateUrl: './lets-work-together.component.html',
+  styleUrl: './lets-work-together.component.css'
+})
+export class LetsWorkTogetherComponent {
+
+}
